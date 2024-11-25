2e6f470269a7c0898f3625374a9b1689b5a1443b
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-11-25 10:10:03 +0100

    updated app.config.ts & created environment variables handling files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,20 +6,21 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideAnimationsAsync(),
     provideFirebaseApp(() => initializeApp({
-      "projectId":"kanban-ca28d",
-      "appId":"1:429632893423:web:d93c861b67726315bc061a",
-      "databaseURL":"https://kanban-ca28d-default-rtdb.firebaseio.com",
-      "storageBucket":"kanban-ca28d.firebasestorage.app",
-      "apiKey":"AIzaSyASU-41wRb8Hfn7FxTErQtv_bjYD6C7zgc",
-      "authDomain":"kanban-ca28d.firebaseapp.com",
-      "messagingSenderId":"429632893423",
-      "measurementId":"G-R0360YZ2R8"
+      "projectId":environment.firebaseProjectId,
+      "appId":environment.firebaseAppId,
+      "databaseURL":environment.firebaseDatabaseURL,
+      "storageBucket":environment.firebaseStorageBucket,
+      "apiKey": environment.firebaseApiKey,
+      "authDomain":environment.firebaseAuthDomain,
+      "messagingSenderId":environment.firebaseMessagingSenderId,
+      "measurementId":environment.firebaseMeasurementId
     })),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase())]
