5caa6e77faf4366e18e878058dcaf1f05f15da74
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-11-25 10:23:36 +0100

    updates

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseApiKey: "AIzaSyASU-41wRb8Hfn7FxTErQtv_bjYD6C7zgc",
+  firebaseAuthDomain: "kanban-ca28d.firebaseapp.com",
+  firebaseDatabaseURL: "https://kanban-ca28d-default-rtdb.firebaseio.com",
+  firebaseProjectId: "kanban-ca28d",
+  firebaseStorageBucket: "kanban-ca28d.firebasestorage.app",
+  firebaseMessagingSenderId: "429632893423",
+  firebaseAppId: "1:429632893423:web:d93c861b67726315bc061a",
+  firebaseMeasurementId: "G-R0360YZ2R8"
+};
