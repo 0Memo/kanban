61d30b21ecf4d66997527e6e895a418b34cb693c
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-01-11 20:20:51 +0100

    updates on environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseApiKey: process.env['apiKey'],
+  firebaseAuthDomain: process.env['authDomain'],
+  firebaseDatabaseURL: process.env['databaseURL'],
+  firebaseProjectId: process.env['projectId'],
+  firebaseStorageBucket: process.env['storageBucket'],
+  firebaseMessagingSenderId: process.env['messagingSenderId'],
+  firebaseAppId: process.env['appId'],
+  firebaseMeasurementId: process.env['measurementId']
+};
