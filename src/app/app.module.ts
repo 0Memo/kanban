306588bd2e6163a21fa942106ca7a7fdeb3f8503
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-01-12 06:47:12 +0100

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterOutlet } from '@angular/router';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AppComponent
+    AppComponent,
+    RouterOutlet
   ],
   providers: [],
 })
