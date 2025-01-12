35556872b71b254bfa8a3ddbbdcd6ca95d6b939f
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-01-12 09:41:51 +0100

    updated environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const firebaseConfig = {
   production: true,
   firebaseConfig: {
-    apiKey: process.env['firebaseApiKey'] || '',
-    authDomain: process.env['firebaseAuthDomain'] || '',
-    databaseURL: process.env['firebaseDatabaseURL'] || '',
-    projectId: process.env['firebaseProjectId'] || '',
-    storageBucket: process.env['firebaseStorageBucket'] || '',
-    messagingSenderId: process.env['firebaseMessagingSenderId'] || '',
-    appId: process.env['firebaseAppId'] || '',
-    measurementId: process.env['firebaseMeasurementId'] || ''
+    apiKey: 'firebaseApiKey',
+    authDomain: 'firebaseAuthDomain',
+    databaseURL: 'firebaseDatabaseURL',
+    projectId: 'firebaseProjectId',
+    storageBucket: 'firebaseStorageBucket',
+    messagingSenderId: 'firebaseMessagingSenderId',
+    appId: 'firebaseAppId',
+    measurementId: 'firebaseMeasurementId'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  firebaseApiKey: process.env['firebaseApiKey'] || '',
-  firebaseAuthDomain: process.env['firebaseAuthDomain'] || '',
-  firebaseDatabaseURL: process.env['firebaseDatabaseURL'] || '',
-  firebaseProjectId: process.env['firebaseProjectId'] || '',
-  firebaseStorageBucket: process.env['firebaseStorageBucket'] || '',
-  firebaseMessagingSenderId: process.env['firebaseMessagingSenderId'] || '',
-  firebaseAppId: process.env['firebaseAppId'] || '',
-  firebaseMeasurementId: process.env['firebaseMeasurementId'] || ''
+  firebaseApiKey: 'firebaseApiKey',
+  firebaseAuthDomain: 'firebaseAuthDomain',
+  firebaseDatabaseURL: 'firebaseDatabaseURL',
+  firebaseProjectId: 'firebaseProjectId',
+  firebaseStorageBucket: 'firebaseStorageBucket',
+  firebaseMessagingSenderId: 'firebaseMessagingSenderId',
+  firebaseAppId: 'firebaseAppId',
+  firebaseMeasurementId: 'firebaseMeasurementId',
 };
